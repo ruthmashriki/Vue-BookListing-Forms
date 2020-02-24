12f3407f22e66ccd9521a565616d22114621e316
ruthmashriki <61411071+ruthmashriki@users.noreply.github.com>
2020-02-24 15:42:33 +0200

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <hr>
+    <select v-model="holding">
+      <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <h2>Filtered Books By Ownership</h2>
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
@@ -10,6 +18,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,11 +29,19 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman",finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
-      ]
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true ,ownership:"borrowed"},
+        { title: "American Gods", author: "Neil Gaiman",finishedReading: false ,ownership:"bought"},
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true ,ownership:"borrowed"}
+      ],
+      filters : ["bought", "borrowed"],
+      holding: "bought"
     };
+    
+  },
+  computed :{
+    filteredBooks(){
+       return _.filter(this.books, ["ownership", this.holding]);
+    }
   },
   components: {
     BookItem,
@@ -35,7 +52,8 @@ export default {
       this.books.push({ 
         title: bookData.bookTitle, 
         author: bookData.bookAuthor, 
-        finishedReading: bookData.finishedReading});
+        finishedReading: bookData.finishedReading,
+        ownership : bookData.ownership });
     }
   }
 };
